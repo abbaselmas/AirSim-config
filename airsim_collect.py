b71b1c8f34fd14f4ed98831a9171b96a39c055e2
Abbas Elmas <abbaselmas@gmail.com>
2024-12-17 11:38:19 +0300

    area arranged with step size etc.

--- airsim_collect.py
+++ airsim_collect.py
@@ -1,70 +1,65 @@
-# In settings.json first activate computer vision mode: 
-# https://github.com/Microsoft/AirSim/blob/main/docs/image_apis.md#computer-vision-mode
-
 import airsim
 import pprint
-import tempfile
 import os
 import time
 import math
 
 pp = pprint.PrettyPrinter(indent=4)
-
 client = airsim.VehicleClient()
-
-airsim.wait_key('Press any key to get camera parameters')
-camera_info = client.simGetCameraInfo("front_center")
-print("Front Center CameraInfo : %s" % (pp.pprint(camera_info)))
 
 airsim.wait_key('Press any key to start getting images')
 tmp_dir = r"D:\GDrive\Drive'ım\Dataset\AirSimNH"
-
-print ("Saving images to %s" % tmp_dir)
+print("Saving images to %s" % tmp_dir)
 try:
-    os.makedirs(tmp_dir)
+    os.makedirs(tmp_dir, exist_ok=True)  # Ensure the root directory exists
 except OSError:
-    if not os.path.isdir(tmp_dir):
-        raise
-    
+    raise
+
 increment = 1
-step = 25
-z = -70
-up = 111 #150+1 for range last element
-down = -110
+step = 40
+z = -40
+up = 160  # Y-axis upper bound
+down = -160  # Y-axis lower bound
 
 for layer in [1, 2, 3]:
-    for y in range(down, up, step):
-        for x in range(down, up, step):
+    serpentine = True  # Used to toggle y-direction
+    for y in range(down, up+1, step):  # Move along x-axis in one direction for the entire layer
+        if serpentine:
+            x_range = range(down, up+1, step)  # Move upwards
+        else:
+            x_range = range(up, down-1, -step)  # Move downwards
+            
+        for x in x_range:
             for pitch in [-120, -90, -60]:
                 for yaw in [-90, 0]:
                     if pitch == -90 and yaw == -90:
                         continue
+                    
+                    # Set vehicle pose
                     client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(x, y, z), airsim.to_quaternion(math.radians(pitch), 0, math.radians(yaw))), True)
                     time.sleep(0.1)
 
+                    # Get image response
                     responses = client.simGetImages([airsim.ImageRequest("front_center", airsim.ImageType.Scene)])
                     response = responses[0]
 
-                    print(pprint.pformat(response.camera_position))
-                    airsim.write_file(os.path.normpath(os.path.join(tmp_dir, "Layer" + str(layer) 
-                                                                            + "_" + str(increment) 
-                                                                            + "_z" + str(z) 
-                                                                            + "_y" + str(y) 
-                                                                            + "_x" + str(x) 
-                                                                            + "_pitch" + str(pitch)
-                                                                            + "_yaw" + str(yaw) + '.png')), response.image_data_uint8)
-                    print(  "Layer" + str(layer) 
-                            + "_" + str(increment) 
-                            + "_z" + str(z) 
-                            + "_y" + str(y) 
-                            + "_x" + str(x) 
-                            + "_pitch" + str(pitch)
-                            + "_yaw" + str(yaw) + '.png')
-                    increment += 1
-    up -= 20
-    down += 20
-    z -= 30
-    step += 5
+                    # Create the necessary folder for each combination of Layer, pitch, and yaw
+                    folder_path = os.path.join(tmp_dir, f"Layer{layer}/pitch{pitch}_yaw{yaw}")
+                    os.makedirs(folder_path, exist_ok=True)  # Ensure the folder is created
 
-# currently reset() doesn't work in CV mode. Below is the workaround
+                    # Save the image to the folder
+                    image_path = os.path.join(folder_path, f"{increment}_z{z}_y{y}_x{x}.png")
+                    airsim.write_file(os.path.normpath(image_path), response.image_data_uint8)
+
+                    print(f"Saved {image_path}")
+                    increment += 1
+        
+        # Toggle y-direction after each x-row to get serpentine effect
+        serpentine = not serpentine
+
+    down += step
+    up -= step
+    z -= step
+
+# Reset vehicle pose
 client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(0, 0, 0)), True)
